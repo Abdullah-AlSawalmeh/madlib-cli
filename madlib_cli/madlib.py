14b6c4cac29532861f1fb26d661cc1177ae0acdb
abdullah-alsawalmeh <abdullahalsawalmeh@gmail.com>
2021-06-09 01:27:17 +0300

    Finish lab 03

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,17 +1,25 @@
 import re
 
+### Global Variables 
 dark_and_storm_path = "assets/dark_and_stormy_night_template.txt"
 make_me_path = "assets/make_me_a_video_game_template.txt"
-
-
+path = "missing.txt"
 
 def read_template(file_path):
-    with open(file_path,'r') as reader:
-        file_content = reader.read()
-        return file_content
+    """
+    Function to read txt files, and return it is content.
+    """
+    try:
+        with open(file_path,'r') as reader:
+            file_content = reader.read()
+            return file_content
+    except FileNotFoundError:
+        return('file not found')
 
 def parse_template(parsed_template):
-    # searched_regex = re.findall(r'\b\w*\b}', parsed_template)
+    """
+    Function to return the content stripped from the placeholders, and return the place holders.
+    """
     searched_regex = re.findall(r'({.+?})', parsed_template)
     for i,j in enumerate(searched_regex):
         j_subbed = re.sub(r'[{}]', '', j)
@@ -21,15 +29,20 @@ def parse_template(parsed_template):
     return [empty_template,searched_regex]
 
 def ask_user(questions):
+    """
+    Function to return user answers.
+    """
     user_answers = []
     for i in questions:
         answer = input(f"May I ask you to provide me {i} ? ")
         user_answers.append(answer)
-    # print(user_answers)
     return user_answers
     
 
 def merge(actual_stripped,user_answers):
+    """
+    Function to return user answers merged with the sripped template.
+    """
     print(user_answers)
     new_text = actual_stripped.format(*user_answers)
     print(new_text)
@@ -38,6 +51,7 @@ def merge(actual_stripped,user_answers):
 
 if __name__ == '__main__':  
     parsed_template = read_template(make_me_path)
+    # parsed_template = read_template(path)
     actual_stripped, actual_parts = parse_template(parsed_template)
     user_answers = ask_user(actual_parts)
     merge(actual_stripped,user_answers)
